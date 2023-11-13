584d62afc0b2c9c33657ea12b3c2a8cd24b69c8c
下平大輝 <shimodairadaiki@shimodairadaiakiranoMacBook-Air.local>
2023-11-13 15:22:51 +0900

    initial commit

---
+++ env/load_env.go
@@ -0,0 +1,27 @@
+package env
+
+import (
+	"fmt"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+type Env struct{}
+
+// 環境変数を読み込むだけ
+
+func (e *Env) LoadEnv(envName string) (string){
+	err := godotenv.Load("./.env")
+	if err != nil{
+		fmt.Println("読み込み失敗")
+		return ""
+	}
+
+	env := os.Getenv(envName)
+	if env == ""{
+		fmt.Println(envName + "が設定されていません")
+		return ""
+	} 
+	
+	return env
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Shimodaira0910/summary
+
+go 1.19
+
+require github.com/joho/godotenv v1.5.1
