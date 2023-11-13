1d5fd9c59c77d32e2149a5f98ae8d709c267d867
下平大輝 <shimodairadaiki@shimodairadaiakiranoMacBook-Air.local>
2023-11-13 15:46:34 +0900

    Successful connection to slack

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/Shimodaira0910/summary
 go 1.19
 
 require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/slack-go/slack v0.12.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import(
-	
+	"fmt"
+	"github.com/slack-go/slack"
+	"github.com/Shimodaira0910/summary/env"
 )
 
-func main(){
+func main(){	
+	env := env.Env{}
+	api := slack.New(env.LoadEnv("SLACK_BOT_TOKEN"))
+	channelId := env.LoadEnv("SLACK_CHANNEL_ID")
 
-}
+	channelId, timestamp, err := api.PostMessage(
+		channelId,
+		slack.MsgOptionText("hello!!", false),
+	)
+	if err != nil{
+		fmt.Println("接続失敗")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Message successfully sent to channel %s at %s", channelId, timestamp)
+}	
